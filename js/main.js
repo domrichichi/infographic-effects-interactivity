3301823784aa56c1e1bbbf94b6361e9e7b032a80
domrichichi <domrichichi@gmail.com>
2016-04-12 16:55:07 -0400

    Added fade animation on phones

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 var $runners = $('.runners');
 var $run = $('.run-1');
 var $runTwo = $('.run-2');
+var $runThree = $('.run-3');
 
 
 // phones
@@ -21,9 +22,14 @@ $runners.on('click', function () {
   $runTwo.toggleClass('run-in-2');
 });
 
+$runners.on('click', function () {
+  $runThree.toggleClass('run-in-3');
+});
+
+// mousein
+
 // phones
 
 $phones.on('click', function () {
-  $redPhones.addClass('make-red');
-
+  $redPhones.toggleClass('make-red');
 })
